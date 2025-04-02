45065f2f0e0701745b498891c411710b836d2f6e
Samarjeet621-stack <samarjeet.singh@newvision-software.com>
2025-04-02 19:26:53 +0530

    Partial Code sended

---
+++ eShopBackendAPI/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using eShopBackendAPI.Data;
+using eShopBackendAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShopBackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(StoreContext _context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct()
+        {
+            return await _context.Products.ToListAsync();  
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            if (products == null) return NotFound();
+            return Ok(products);
+        }
+    }
+}
